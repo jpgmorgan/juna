487349df6cbab1e276fb53a58de0027e43110cd0
swissquant@gmail.com <aym.germain@gmail.com>
2023-12-01 10:22:06 +0000

    adding GondiClient in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { WETH, DAI, USDC, currencyFromAddress } from "./support/currencies";
 export { ArcadeClient } from "./arcade/ArcadeClient";
 export { NftfiClient } from "./nftfi/NFTfiClient";
+export { GondiClient } from "./gondi/GondiClient";
 export { PortfolioClient } from "./support/PortfolioClient";
 export * as types from "./types";
