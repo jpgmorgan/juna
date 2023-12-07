85337838ef98d37fcae76128ca55dcee093c5232
swissquant@gmail.com <aym.germain@gmail.com>
2023-12-07 15:04:08 -0500

    loaner

--- src/support/loaner.ts
+++ src/support/loaner.ts
@@ -1,1 +1,25 @@
-export class Loaner {}
+import { PortfolioClient } from "..";
+
+type Offer = { ltv: number; durationInDays: number };
+type CollectionOffers = Record<`0x${string}`, Offer[]>;
+type SingleItemOffers = Record<`0x${string}`, { ids: number[]; multiplier: number; offers: Offer[] }[]>;
+
+export class Loaner {
+  private readonly portfolioClient: PortfolioClient;
+  private singleItemOffers: SingleItemOffers;
+  private collectionOffers: CollectionOffers;
+
+  constructor(portfolioClient: PortfolioClient) {
+    this.portfolioClient = portfolioClient;
+    this.singleItemOffers = {};
+    this.collectionOffers = {};
+  }
+
+  public addCollectionOffers(collectionAddress: `0x${string}`, offers: Offer[]) {
+    this.collectionOffers[collectionAddress] = offers;
+  }
+
+  public publishCollectionOffers() {
+    throw Error("Not implemented");
+  }
+}
