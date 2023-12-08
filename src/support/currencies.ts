fdeae79423e71eb6df542c6f86ecc2991e9151fe
swissquant@gmail.com <aym.germain@gmail.com>
2023-12-08 13:49:01 -0500

    currencies: fix typo

--- src/support/currencies.ts
+++ src/support/currencies.ts
@@ -8,7 +8,7 @@ export const WETH: Currency = {
 
 export const DAI: Currency = {
   address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-  symbol: "WETH",
+  symbol: "DAI",
   decimals: 18,
 };
 
