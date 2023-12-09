0e9f7e43a5f36cc021a4df554757a0754a63776d
swissquant@gmail.com <aym.germain@gmail.com>
2023-12-09 10:37:54 -0500

    loaner improvement

--- src/support/loaner.ts
+++ src/support/loaner.ts
@@ -1,11 +1,9 @@
-import { PortfolioClient, WETH } from "..";
+import { PortfolioClient } from "..";
 import { Currency } from "../types";
 import { CollectionRegistry } from "./CollectionRegistry";
 
-type LoanTerm = { currency: Currency; ltv: number; durationInDays: number; apr: number };
-type BasePrice = { eth: number; usd: number };
-type CollectionOffer = { basePrice: BasePrice; loanTerms: LoanTerm[] };
-type CollectionOffers = Record<`0x${string}`, CollectionOffer>;
+type LoanTerm = { currency: Currency; principal: number; durationInDays: number; apr: number };
+type CollectionOffers = Record<`0x${string}`, LoanTerm[]>;
 
 export class Loaner {
   private readonly portfolioClient: PortfolioClient;
@@ -16,46 +14,25 @@ export class Loaner {
     this.collectionOffers = {};
   }
 
-  public updateCollectionOffers(
-    collectionAddress: `0x${string}`,
-    basePriceInEth: number,
-    basePriceInUsd: number,
-    loanTerms: LoanTerm[],
-  ) {
-    this.collectionOffers[collectionAddress] = {
-      basePrice: { eth: basePriceInEth, usd: basePriceInUsd },
-      loanTerms: loanTerms,
-    };
-  }
-
-  private getBasePriceForCurrency(basePrice: BasePrice, currency: Currency) {
-    if ([WETH].includes(currency)) {
-      return basePrice.eth;
-    } else {
-      return basePrice.usd;
-    }
+  public updateCollectionOffers(collectionAddress: `0x${string}`, loanTerms: LoanTerm[]) {
+    this.collectionOffers[collectionAddress] = loanTerms;
   }
 
   public async publishCollectionOffers(expiryInMinutes: number = 10) {
-    for (const [collectionAddress, offering] of Object.entries(this.collectionOffers)) {
+    for (const [collectionAddress, loanTerms] of Object.entries(this.collectionOffers)) {
       const collectionName = CollectionRegistry.getCollectionDetails(collectionAddress as `0x${string}`).name;
-      console.log(`${collectionName}: base_price = ${offering.basePrice.eth} eth | ${offering.basePrice.usd} usd`);
-
-      for (const loanTerm of offering.loanTerms) {
-        const principal = loanTerm.ltv * this.getBasePriceForCurrency(offering.basePrice, loanTerm.currency);
-
+      for (const loanTerm of loanTerms) {
         console.log(
-          `publishing collection offer on ${collectionName}: ltv=${
-            loanTerm.ltv * 100
-          }% | principal=${principal} ${loanTerm.currency.symbol.toLowerCase()} | duration=${
-            loanTerm.durationInDays
-          } days | apr=${loanTerm.apr * 100}%`,
+          `publishing collection offer on ${collectionName}: principal=${
+            loanTerm.principal
+          } ${loanTerm.currency.symbol.toLowerCase()} | duration=${loanTerm.durationInDays} days | apr=${
+            loanTerm.apr * 100
+          }%`,
         );
-
         await this.portfolioClient.createCollectionOffer({
           collectionAddress: collectionAddress as `0x${string}`,
           currency: loanTerm.currency,
-          principal: principal,
+          principal: loanTerm.principal,
           apr: loanTerm.apr,
           durationInDays: loanTerm.durationInDays,
           expiryInMinutes: expiryInMinutes,
